5e1b46338c3c3048de742220060772fda347a1a0
Robert Ende <robert@ende.pro>
2022-10-08 14:11:56 +0200

    feat: implement working wasm_pack_renderer

--- wasm-pack-renderer/src/lib.rs
+++ wasm-pack-renderer/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
+use simulation::sand_sim::Simulation;
+use simulation::universe::{Cell, Universe};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +12,49 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct WasmPackRenderer {
+    sim: Simulation,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-pack-renderer!");
+impl WasmPackRenderer {
+    pub fn new(width: usize, height: usize) -> Self {
+        let mut sim = Simulation::new(width, height);
+        sim.universe.area[width / 2] = Cell::WaterGenerator;
+        Self { sim }
+    }
+
+    pub fn tick(&mut self) {
+        self.sim.tick();
+    }
+
+    pub fn get_data(&self) -> Clamped<Vec<u8>> {
+        Clamped(to_u8(&self.sim.universe))
+    }
+}
+
+const AIR_COLOR: [u8; 4] = [0xff, 0xff, 0xff, 0xff];
+const WATER_COLOR: [u8; 4] = [0, 0, 0xff, 0xff];
+const SAND_COLOR: [u8; 4] = [0xff, 0xff, 0, 0xff];
+const WATER_GENERATOR_COLOR: [u8; 4] = [0, 0xff, 0xff, 0xff];
+const SAND_GENERATOR_COLOR: [u8; 4] = [0xff, 0, 0xff, 0xff];
+
+fn to_u8(universe: &Universe) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::with_capacity(universe.area.len() * 4);
+
+    for cell in &universe.area {
+        let color = match cell {
+            Cell::Sand => &SAND_COLOR,
+            Cell::SandGenerator => &SAND_GENERATOR_COLOR,
+            Cell::Water => &WATER_COLOR,
+            Cell::WaterGenerator => &WATER_GENERATOR_COLOR,
+            Cell::Air => &AIR_COLOR,
+        };
+
+        out.extend_from_slice(color);
+
+        // out[4 * i..4 * i + 4].copy_from_slice(color);
+    }
+
+    out
 }
--- wasm-pack-renderer/www/index.js
+++ wasm-pack-renderer/www/index.js
@@ -1,3 +1,24 @@
-import * as wasm from "hello-wasm-pack";
+import {WasmPackRenderer} from "wasm-pack-renderer"
 
-wasm.greet();
+
+const canvas = document.getElementById("sandspiel-canvas");
+const ctx = canvas.getContext('2d');
+
+const width = 200;
+const height = 200;
+
+const renderer = WasmPackRenderer.new(width, height);
+
+const draw = () => {
+    let data = renderer.get_data();
+    let imgData = new ImageData(data, width, height);
+    ctx.putImageData(imgData, 0, 0);
+}
+
+const renderLoop = () => {
+    draw();
+    renderer.tick();
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
