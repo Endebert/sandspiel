5a2f7356fbfe802b26cf4925ddd547ed59fddfbf
Robert Ende <robert@ende.pro>
2022-10-10 16:35:28 +0200

    fix: fix bugs in get_neighbor_pos
    fix sand collision with water
    refactor Position type to struct
    refactor Velocity struct to type

--- simulation/src/sand_sim.rs
+++ simulation/src/sand_sim.rs
@@ -1,4 +1,6 @@
-use crate::universe::{Cell, CellKind, Direction, Position, Universe, Velocity};
+use crate::universe::CellKind::*;
+use crate::universe::Direction::*;
+use crate::universe::{Cell, Position, Universe, Velocity};
 use rand::random;
 
 pub struct Simulation {
@@ -16,15 +18,15 @@ impl Simulation {
     pub fn tick(&mut self) {
         self.universe.set_all_unhandled();
 
-        for pos in (0..self.universe.area.len()).rev() {
-            self.handle_cell_at(pos);
+        for index in (0..self.universe.area.len()).rev() {
+            self.handle_cell_at(self.universe.i_to_pos(index), false);
         }
     }
 
-    fn handle_cell_at(&mut self, pos: Position) {
-        let cell = self.universe.get_cell_mut(pos).unwrap();
+    fn handle_cell_at(&mut self, pos: Position, force: bool) {
+        let cell = self.universe.get_cell_mut(&pos).unwrap();
 
-        if cell.handled {
+        if cell.handled && !force {
             return;
         }
 
@@ -33,66 +35,62 @@ impl Simulation {
         let initial_velocity = cell.velocity.clone();
 
         let (final_pos, final_velocity) = match cell.kind {
-            CellKind::Air => self.handle_air(pos, initial_velocity),
-            CellKind::Sand => self.handle_sand(pos, initial_velocity),
-            CellKind::SandGenerator => self.handle_sand_generator(pos, initial_velocity),
-            CellKind::Water => self.handle_water(pos, initial_velocity),
-            CellKind::WaterGenerator => self.handle_water_generator(pos, initial_velocity),
+            Air => self.handle_air(pos, initial_velocity),
+            Sand => self.handle_sand(pos, initial_velocity),
+            SandGenerator => self.handle_sand_generator(pos, initial_velocity),
+            Water => self.handle_water(pos, initial_velocity),
+            WaterGenerator => self.handle_water_generator(pos, initial_velocity),
         };
 
-        self.universe.get_cell_mut(final_pos).unwrap().velocity = final_velocity;
+        self.universe.get_cell_mut(&final_pos).unwrap().velocity = final_velocity;
     }
 
     fn handle_sand(&mut self, pos: Position, velocity: Velocity) -> (Position, Velocity) {
-        let mut current_pos = pos.clone();
-        'outer_for: for _ in 0..velocity.x.abs() {
-            for dir in [Direction::Down, Direction::RightDown, Direction::LeftDown] {
+        let mut current_pos = pos;
+        let mut moves = velocity.abs() + 1;
+
+        'outer_loop: while moves > 0 {
+            moves -= 1;
+            'inner_loop: for dir in [Down, RightDown, LeftDown] {
                 if let Some((other_cell, other_pos)) =
                     self.universe.get_neighbor_mut(&current_pos, &dir)
                 {
                     match other_cell.kind {
-                        CellKind::Air => {
-                            self.universe.swap_cells(current_pos, other_pos);
+                        Air => {
+                            self.universe.swap_cells(&current_pos, &other_pos);
                             current_pos = other_pos;
-                            continue 'outer_for;
+                            continue 'outer_loop;
                         }
-                        CellKind::Water => {
-                            self.universe.swap_cells(current_pos, other_pos);
+                        Water => {
+                            // if dir == Down {
+                            self.universe.swap_cells(&current_pos, &other_pos);
                             // want the switched water cell to be handled as well, as it might have been put past the iteration cursor
-                            self.handle_cell_at(current_pos);
+                            self.handle_cell_at(current_pos, true);
                             current_pos = other_pos;
-                            continue 'outer_for;
+                            break 'inner_loop;
                         }
                         _ => {}
                     }
                 }
             }
-            return (current_pos, Velocity::new());
+            return (current_pos, 0);
         }
-        return (
-            current_pos,
-            Velocity {
-                x: velocity.x + 1,
-                y: velocity.y,
-            },
-        );
+
+        (current_pos, velocity + 1)
     }
 
     fn handle_water(&mut self, pos: Position, velocity: Velocity) -> (Position, Velocity) {
-        let mut current_pos = pos.clone();
-        'outer_loop: for _ in 0..velocity.x.abs() {
-            for dir in [
-                Direction::Down,
-                Direction::RightDown,
-                Direction::LeftDown,
-                Direction::Right,
-                Direction::Left,
-            ] {
-                if let Some((other_cell, other_pos)) = self.universe.get_neighbor(current_pos, dir)
+        let mut current_pos = pos;
+        let mut moves = velocity.abs() + 1;
+        'outer_loop: while moves > 0 {
+            moves -= 1;
+            for dir in [Down, RightDown, LeftDown, Right, Left] {
+                if let Some((other_cell, other_pos)) =
+                    self.universe.get_neighbor(&current_pos, &dir)
                 {
                     match other_cell.kind {
-                        CellKind::Air => {
-                            self.universe.swap_cells(current_pos, other_pos);
+                        Air => {
+                            self.universe.swap_cells(&current_pos, &other_pos);
                             current_pos = other_pos;
                             continue 'outer_loop;
                         }
@@ -100,25 +98,19 @@ impl Simulation {
                     }
                 }
             }
-            return (current_pos, Velocity::new());
+            return (current_pos, 0);
         }
-        return (
-            current_pos,
-            Velocity {
-                x: velocity.x + 1,
-                y: velocity.y,
-            },
-        );
+
+        (current_pos, velocity + 1)
     }
 
     fn handle_sand_generator(&mut self, pos: Position, velocity: Velocity) -> (Position, Velocity) {
-        for dir in [Direction::Down] {
-            if let Some((other_cell, other_pos)) = self.universe.get_neighbor(pos, dir) {
+        for dir in [Down] {
+            if let Some((other_cell, other_pos)) = self.universe.get_neighbor(&pos, &dir) {
                 match other_cell.kind {
-                    CellKind::Air => {
+                    Air => {
                         if random() {
-                            self.universe
-                                .set_cell(Cell::new(CellKind::Sand, true), other_pos);
+                            self.universe.set_cell(Cell::new(Sand, true), &other_pos);
                             break;
                         }
                     }
@@ -134,13 +126,12 @@ impl Simulation {
         pos: Position,
         velocity: Velocity,
     ) -> (Position, Velocity) {
-        for dir in [Direction::Down] {
-            if let Some((other_cell, other_pos)) = self.universe.get_neighbor(pos, dir) {
+        for dir in [Down] {
+            if let Some((other_cell, other_pos)) = self.universe.get_neighbor(&pos, &dir) {
                 match other_cell.kind {
-                    CellKind::Air => {
+                    Air => {
                         if random() {
-                            self.universe
-                                .set_cell(Cell::new(CellKind::Water, true), other_pos);
+                            self.universe.set_cell(Cell::new(Water, true), &other_pos);
                             break;
                         }
                     }
--- simulation/src/universe.rs
+++ simulation/src/universe.rs
@@ -1,3 +1,5 @@
+use crate::universe::Direction::*;
+
 pub struct Universe {
     pub area: Vec<Cell>,
     pub width: usize,
@@ -23,34 +25,46 @@ impl Universe {
         }
     }
 
-    pub fn get_cell(&self, pos: Position) -> Option<&Cell> {
-        self.area.get(pos)
+    pub(crate) fn get_cell(&self, pos: &Position) -> Option<&Cell> {
+        self.area.get(self.pos_to_i(pos))
     }
 
-    pub fn get_cell_mut(&mut self, pos: Position) -> Option<&mut Cell> {
-        self.area.get_mut(pos)
+    pub(crate) fn get_cell_mut(&mut self, pos: &Position) -> Option<&mut Cell> {
+        let index = self.pos_to_i(pos);
+        self.area.get_mut(index)
     }
 
-    pub fn set_cell(&mut self, cell: Cell, pos: Position) {
-        self.area[pos] = cell;
+    pub(crate) fn set_cell(&mut self, cell: Cell, pos: &Position) {
+        let index = self.pos_to_i(pos);
+        self.area[index] = cell;
     }
 
-    pub fn swap_cells(&mut self, pos1: Position, pos2: Position) {
-        self.area.swap(pos1, pos2);
+    pub(crate) fn swap_cells(&mut self, pos1: &Position, pos2: &Position) {
+        let index1 = self.pos_to_i(pos1);
+        let index2 = self.pos_to_i(pos2);
+        self.area.swap(index1, index2);
     }
 
-    pub fn get_neighbor(&self, pos: Position, dir: Direction) -> Option<(&Cell, Position)> {
-        self.get_neighbor_pos(&pos, &dir)
-            .map(|other_pos| (self.get_cell(other_pos).unwrap(), other_pos))
+    pub(crate) fn get_neighbor(
+        &self,
+        pos: &Position,
+        dir: &Direction,
+    ) -> Option<(&Cell, Position)> {
+        let neighbor_pos = self.get_neighbor_pos(&pos, dir)?;
+        let neighbor = self.get_cell(&neighbor_pos)?;
+
+        Some((neighbor, neighbor_pos))
     }
 
-    pub fn get_neighbor_mut(
+    pub(crate) fn get_neighbor_mut(
         &mut self,
         pos: &Position,
         dir: &Direction,
     ) -> Option<(&mut Cell, Position)> {
-        self.get_neighbor_pos(&pos, &dir)
-            .map(|other_pos| (self.get_cell_mut(other_pos).unwrap(), other_pos))
+        let neighbor_pos = self.get_neighbor_pos(&pos, &dir)?;
+        let neighbor = self.get_cell_mut(&neighbor_pos)?;
+
+        Some((neighbor, neighbor_pos))
     }
 
     pub fn set_all_unhandled(&mut self) {
@@ -60,54 +74,44 @@ impl Universe {
     }
 
     fn get_neighbor_pos(&self, pos: &Position, dir: &Direction) -> Option<Position> {
-        let pos_x = pos % self.height;
-
-        let offset_hor: isize = match dir {
-            Direction::Left | Direction::LeftUp | Direction::LeftDown => -1,
-            Direction::Right | Direction::RightUp | Direction::RightDown => 1,
-            _ => 0,
+        let Position { x, y } = *pos;
+        let x = match dir {
+            Left | LeftUp | LeftDown => x.checked_sub(1)?,
+            Right | RightUp | RightDown => x.checked_add(1)?,
+            _ => x,
         };
 
-        let offset_ver: isize = match dir {
-            Direction::Up | Direction::LeftUp | Direction::RightUp => -self.signed_width(),
-            Direction::Down | Direction::LeftDown | Direction::RightDown => self.signed_width(),
-            _ => 0,
+        let y = match dir {
+            Up | LeftUp | RightUp => y.checked_sub(1)?,
+            Down | LeftDown | RightDown => y.checked_add(1)?,
+            _ => y,
         };
 
-        let x: isize = isize::try_from(pos_x).unwrap() + offset_hor;
-
-        if 0 < x && x < self.signed_width() {
-            let offset = offset_hor + offset_ver;
-
-            if offset.is_positive() {
-                // add offset to pos and check if it is still in bounds of area
-                pos.checked_add(offset.unsigned_abs())
-                    .filter(|x| *x < self.area.len())
-            } else {
-                pos.checked_sub(offset.unsigned_abs())
-            }
+        if x < self.width && y < self.height {
+            Some(Position { x, y })
         } else {
             None
         }
     }
 
-    fn signed_width(&self) -> isize {
-        self.width.try_into().unwrap()
+    pub(crate) fn i_to_pos(&self, index: usize) -> Position {
+        let x = index % self.width;
+        let y = (index - x) / self.width;
+
+        Position { x, y }
+    }
+
+    pub(crate) fn pos_to_i(&self, position: &Position) -> usize {
+        let Position { x, y } = position;
+
+        y * self.width + x
     }
 }
-
-pub type Position = usize;
 
 #[derive(Clone)]
-pub struct Velocity {
-    pub x: i8,
-    pub y: i8,
-}
-
-impl Velocity {
-    pub fn new() -> Self {
-        Self { x: 0, y: 0 }
-    }
+pub(crate) struct Position {
+    pub x: usize,
+    pub y: usize,
 }
 
 #[derive(Clone, PartialEq)]
@@ -132,6 +136,8 @@ pub enum CellKind {
     // Solid,
 }
 
+pub type Velocity = i8;
+
 #[derive(Clone)]
 pub struct Cell {
     pub kind: CellKind,
@@ -143,8 +149,21 @@ impl Cell {
     pub fn new(kind: CellKind, handled: bool) -> Self {
         Self {
             kind,
-            velocity: Velocity::new(),
+            velocity: 0,
             handled,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cannot_get_neighor_right_on_edge() {
+        let universe = Universe::new(3, 3);
+        assert!(universe
+            .get_neighbor(&Position { x: 2, y: 1 }, &Right)
+            .is_none());
     }
 }
